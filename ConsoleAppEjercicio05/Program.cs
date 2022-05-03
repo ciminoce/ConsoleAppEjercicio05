54fa0ec731e009af131904c301e51b71d67fc474
Carlos Cimino <ciminoce@yahoo.com.ar>
2022-05-02 21:44:04 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleAppEjercicio05/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleAppEjercicio05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radio;
+            double perimetro, superficie;
+            Console.Title = "Datos de la Circunferencia";
+            Console.Write("Ingrese el radio de la circunferencia:");
+            radio = double.Parse(Console.ReadLine());
+
+            perimetro = 2 * radio * Math.PI;
+            superficie = Math.PI * Math.Pow(radio, 2);
+
+            Console.WriteLine($"Superficie de circ de radio {radio} es {superficie:N3}");
+            Console.WriteLine($"El perímetro de una circ de radio {radio} es {perimetro:N2}");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
